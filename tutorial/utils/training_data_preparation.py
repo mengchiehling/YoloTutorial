0c07748d967219789a5bffaabc0c1aabcccc9b84
Meng-Chieh Ling <meng.ling@check24.de>
2021-08-14 21:40:33 +0200

    Update

--- tutorial/utils/training_data_preparation.py
+++ tutorial/utils/training_data_preparation.py
@@ -1,33 +1,37 @@
 import argparse
 import os
-import multiprocessing
 from glob import glob
-from shutil import copyfile
+from shutil import copyfile, move
 from typing import List
 
 import numpy as np
-from pathos.multiprocessing import ProcessingPool as Pool
 
 from tutorial.io.path_definition import get_project_dir
 
 
 def file_transfer(new_train: str, jpg_files: List[str], txt_files: List[str], classname_file: str):
 
-    new_train_dir = f"{root_folder}/training/{new_train}"
+    new_train_dir = f"{root_folder}/train/{new_train}"
 
     if not os.path.isdir(new_train_dir):
         os.makedirs(new_train_dir)
 
     for ix, (src_jpg_file, src_txt_file) in enumerate(zip(jpg_files, txt_files)):
 
+        basename = os.path.basename(src_txt_file)
+        classname = basename.split("_")[0]
+
         p = np.random.rand()
 
         if p > 0.9:
-            dst_jpg_file = f"{new_train_dir}/image_val_{ix:04d}.jpg"
-            dst_txt_file = f"{new_train_dir}/image_val_{ix:04d}.txt"
+            dst_jpg_file = f"{new_train_dir}/{classname}_test_{ix:04d}.jpg"
+            dst_txt_file = f"{new_train_dir}/{classname}_test_{ix:04d}.txt"
+        elif (p <= 0.9) and (p > 0.8):
+            dst_jpg_file = f"{new_train_dir}/{classname}_val_{ix:04d}.jpg"
+            dst_txt_file = f"{new_train_dir}/{classname}_val_{ix:04d}.txt"
         else:
-            dst_jpg_file = f"{new_train_dir}/image_train_{ix:04d}.jpg"
-            dst_txt_file = f"{new_train_dir}/image_train_{ix:04d}.txt"
+            dst_jpg_file = f"{new_train_dir}/{classname}_train_{ix:04d}.jpg"
+            dst_txt_file = f"{new_train_dir}/{classname}_train_{ix:04d}.txt"
 
         copyfile(src_jpg_file, dst_jpg_file)
         copyfile(src_txt_file, dst_txt_file)
@@ -35,18 +39,35 @@ def file_transfer(new_train: str, jpg_files: List[str], txt_files: List[str], cl
     copyfile(classname_file, f"{new_train_dir}/classes.names")
 
 
+def renaming_process():
+
+    files = glob(f"{root_folder}/raw/{args.NEW_DATA}/*")
+
+    for f in files:
+        try:
+            name, code, ext = f.split(".")
+            new_filename = "_".join([name, f"{int(code):04d}"])
+            new_filename += f".{ext.lower()}"
+            move(f, new_filename)
+            os.remove(f)
+        except:
+            pass
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--previous_train', dest="PREVIOUS_TRAIN", type=str)
-    parser.add_argument('--new_data', dest='NEW_DATA', type=str)
+    parser.add_argument('--new_data', dest="NEW_DATA", type=str)
     parser.add_argument('--new_train', dest="NEW_TRAIN", type=str)
     args = parser.parse_args()
 
-    root_folder = f"{get_project_dir()}/data/train/YOLO"
+    root_folder = f"{get_project_dir()}/data"
+
+    renaming_process()
 
     if args.PREVIOUS_TRAIN:
-        train_jpg_files = glob(f"{root_folder}/training/{args.PREVIOUS_TRAIN}/*.jpg")
+        train_jpg_files = glob(f"{root_folder}/train/{args.NEW_TRAIN}/*.jpg")
         train_txt_files = [f.replace("jpg", "txt") for f in train_jpg_files]
     else:
         train_jpg_files = []
